2cdf903fdf4155be7d5b95c055927a76ea09a3c6
Lew <44101962+lew-scott@users.noreply.github.com>
2020-05-30 14:26:02 +0100

    Example working for reading a grid
    png needs to bigger than 900 x 900 pixels to work effectively

---
+++ reader.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat May 30 11:49:24 2020
+
+@author: pm11lms
+"""
+
+
+import cv2
+import numpy as np
+import time 
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r"C:\\Users\pm11lms\AppData\Local\Tesseract-OCR\tesseract.exe"
+
+start = time.clock()
+im = cv2.resize(cv2.imread('./test9x9.png'), (1080, 1080))
+out = np.zeros((9, 9), dtype=np.uint8)
+
+for x in range(9):
+    for y in range(9):
+        num = pytesseract.image_to_string(im[10 + x*120:(x+1)*120 - 10, 10 + y*120:(y+1)*120 - 10, :], lang ='eng', config='--psm 8 --oem 1 -c tessedit_char_whitelist=0123456789')
+        if num:
+            out[x, y] = num
+            
+end = time.clock() - start
+print("Time taken to solve: " + str(end))
